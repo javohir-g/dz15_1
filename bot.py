9fe248dbda22dce73aaa1cb54124eced64c33501
javohir-g <807gjb@gmail.com>
2024-11-01 01:59:19 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,35 +1,29 @@
 import telebot
 from buttons import start_buttons
 
-# Укажите ваш API-ключ от BotFather
 bot = telebot.TeleBot('7927478236:AAEaWaz1v2rNK9W5Oc2cZ7PPRjDhaZZMUHk')
 
-# Константа для курса обмена
 USD_TO_UZS_RATE = 12000
 
-# Словарь для временного хранения данных пользователя
 user_data = {}
 
-# Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def start_conversion(message):
     user_id = message.from_user.id
-    user_data[user_id] = {}  # Инициализируем временные данные пользователя
+    user_data[user_id] = {}
     bot.send_message(message.chat.id, "Введите сумму для конвертации:")
     bot.register_next_step_handler(message, get_amount)
 
-# Получение суммы от пользователя
 def get_amount(message):
     user_id = message.from_user.id
     try:
         amount = float(message.text)
-        user_data[user_id]['amount'] = amount  # Сохраняем сумму
+        user_data[user_id]['amount'] = amount
         bot.send_message(message.chat.id, "Выберите направление конверсии:", reply_markup=start_buttons())
     except ValueError:
         bot.send_message(message.chat.id, "Пожалуйста, введите корректную сумму.")
         bot.register_next_step_handler(message, get_amount)
 
-# Обработчик для выбора направления конверсии
 @bot.message_handler(func=lambda message: message.text in ["USD to UZS", "UZS to USD"])
 def convert_currency(message):
     user_id = message.from_user.id
@@ -46,5 +40,4 @@ def convert_currency(message):
         result = amount / USD_TO_UZS_RATE
         bot.send_message(message.chat.id, f"{amount} UZS = {result} USD")
 
-# Запуск бота
 bot.polling()
